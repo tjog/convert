705bc23dee7b6cf97c6c1da9391a3cf8da0de438
tjog <28024277+tjog@users.noreply.github.com>
2024-03-12 13:32:40 +0100

    fix cloudflare pages headers

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      // Cloudflare Pages configuration for cross-origin isolation
+      { src: "_headers", dest: "" },
+    ],
+
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
